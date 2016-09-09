bb70f254a78c25bf88846d20aef2244b0943d2d6
Will Stuckey <guyfleeman@gmail.com>
2016-09-09 00:34:12 -0400

    added checkstyle

--- fp-frontend/src/main/java/frontpage/FXMain.java
+++ fp-frontend/src/main/java/frontpage/FXMain.java
@@ -6,18 +6,38 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
+ * Application entry point.
+ *
  * @author willstuckey
- * @date 9/7/16 <p></p>
  */
-public class FXMain extends Application
-{
-	public static void main(String[] args) {
-		launch(args);
-	}
+public class FXMain extends Application {
+    /**
+     * initial resolution
+     */
+    private static final int RES_WIDTH = 640;
 
-	public void start(Stage primaryStage) {
-		Scene testScene = new Scene(new Label("TEST"), 640, 480);
-		primaryStage.setScene(testScene);
-		primaryStage.show();
-	}
+    /**
+     * initial resolution
+     */
+    private static final int RES_HEIGHT = 480;
+
+    /**
+     * main
+     *
+     * @param args args
+     */
+    public static void main(final String[] args) {
+        launch(args);
+    }
+
+    /**
+     * FX entry point
+     *
+     * @param primaryStage default stage
+     */
+    public final void start(final Stage primaryStage) {
+        Scene testScene = new Scene(new Label("TEST"), RES_WIDTH, RES_HEIGHT);
+        primaryStage.setScene(testScene);
+        primaryStage.show();
+    }
 }
