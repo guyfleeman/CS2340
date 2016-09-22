f1f020921605696c5cb3387e683adec914d7bc22
george-tang <george.x.tang@gmail.com>
2016-09-22 16:15:12 -0400

    added constructor and getters/setters to user class

--- fp-frontend/src/main/java/model/User.java
+++ fp-frontend/src/main/java/model/User.java
@@ -1,7 +1,32 @@
 package model;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 /**
  * Created by George on 9/20/2016.
  */
 public class User {
+
+    private final StringProperty _username = new SimpleStringProperty();
+    private final StringProperty _password = new SimpleStringProperty();
+
+    public User(String name, String pass) {
+        _username.set(name);
+        _password.set(pass);
+    }
+
+    public String getUsername() {
+        return _username.get();
+    }
+    public void setUsername(String name) {
+        _username.set(name);
+    }
+    public String getPassword() {
+        return _password.get();
+    }
+    public void setPassword(String pass) {
+        _password.set(pass);
+    }
 }
