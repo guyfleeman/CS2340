f3c2f8a4195f4c13bd90364be7a202ebe339a722
Will Stuckey <guyfleeman@gmail.com>
2016-11-12 18:25:16 -0500

    Scene Controller Entry passing checkstyle

--- fp-bind/src/main/java/frontpage/utils/SceneControllerEntry.java
+++ fp-bind/src/main/java/frontpage/utils/SceneControllerEntry.java
@@ -4,7 +4,9 @@ import javafx.scene.Scene;
 
 /**
  * @author willstuckey
- * <p></p>
+ * <p>Duple containing a scene and its controller</p>
+ *
+ * @param <C> controller class
  */
 @SuppressWarnings("WeakerAccess")
 public class SceneControllerEntry<C> {
@@ -12,38 +14,76 @@ public class SceneControllerEntry<C> {
     private C controller;
     private Class<C> controllerClass;
 
-    public SceneControllerEntry(Scene scene, C controller) {
+    /**
+     * creates a scene controller duple
+     * @param scene scene
+     * @param controller controller
+     */
+    public SceneControllerEntry(final Scene scene,
+                                final C controller) {
         this(scene, controller, null);
     }
 
+    /**
+     * creates a scene controller duple
+     * @param scene scene
+     * @param controller controller
+     * @param controllerClass controller class
+     */
     @SuppressWarnings("SameParameterValue")
-    public SceneControllerEntry(Scene scene, C controller, Class<C> controllerClass) {
+    public SceneControllerEntry(final Scene scene,
+                                final C controller,
+                                final Class<C> controllerClass) {
         this.scene = scene;
         this.controller = controller;
         this.controllerClass = controllerClass;
     }
 
+    /**
+     * gets the scene
+     * @return scene
+     */
     public Scene getScene() {
         return scene;
     }
 
-    public void setScene(Scene scene) {
+    /**
+     * sets the scene
+     * @param scene scene
+     */
+    public void setScene(final Scene scene) {
         this.scene = scene;
     }
 
+    /**
+     * gets the controller
+     * @return controller
+     */
     public C getController() {
         return controller;
     }
 
-    public void setController(C controller) {
+    /**
+     * sets the controller
+     * @param controller controller
+     */
+    public void setController(final C controller) {
         this.controller = controller;
     }
 
+    /**
+     * gets controller class type
+     * @return controller class type
+     */
     public Class<C> getControllerClass() {
         return controllerClass;
     }
 
-    public void setControllerClass(Class<C> controllerClass) {
+    /**
+     * sets the controller class
+     * @param controllerClass controller class
+     */
+    public void setControllerClass(final Class<C> controllerClass) {
         this.controllerClass = controllerClass;
     }
 }
