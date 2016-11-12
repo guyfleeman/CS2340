849228a3797d51ca0f07363f4a8b0001d28c5477
Will Stuckey <guyfleeman@gmail.com>
2016-11-12 18:43:19 -0500

    Water Type now passing checkstyle

--- fp-bind/src/main/java/frontpage/model/report/WaterType.java
+++ fp-bind/src/main/java/frontpage/model/report/WaterType.java
@@ -2,14 +2,41 @@ package frontpage.model.report;
 
 /**
  * @author George
- * <p></p>
+ * <p>Water type</p>
  */
 public enum WaterType {
+    /**
+     * bottled
+     */
     BOTTLED,
+
+    /**
+     * well
+     */
     WELL,
+
+    /**
+     * stream
+     */
     STREAM,
+
+    /**
+     * lake
+     */
     LAKE,
+
+    /**
+     * spring
+     */
     SPRING,
+
+    /**
+     * other
+     */
     OTHER,
+
+    /**
+     * unavailable
+     */
     UNAVAILABLE
 }
