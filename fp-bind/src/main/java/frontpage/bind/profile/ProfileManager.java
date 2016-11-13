d9fca23ded45f2674e97db761d4f459aab3281e7
Will Stuckey <guyfleeman@gmail.com>
2016-11-12 20:04:40 -0500

    Profile Manager now passing checkstyle

--- fp-bind/src/main/java/frontpage/bind/profile/ProfileManager.java
+++ fp-bind/src/main/java/frontpage/bind/profile/ProfileManager.java
@@ -6,13 +6,28 @@ import java.util.Map;
 
 /**
  * @author willstuckey
- * <p></p>
+ * <p>Profile Manager</p>
  */
 public interface ProfileManager {
+    /**
+     * gets profile properties from the backend
+     * @param email email for auth
+     * @param tok token for auth
+     * @return profile properties
+     * @throws ProfileManagementException if something goes wrong
+     */
     Map<String, String> getProfile(final String email,
                                    final String tok)
             throws ProfileManagementException;
 
+    /**
+     * set a profile on the backend
+     * @param email email for auth
+     * @param tok token for auth
+     * @param profiles profile properties
+     * @return success
+     * @throws ProfileManagementException if something goes wrong
+     */
     @SuppressWarnings({"UnusedReturnValue", "SameReturnValue"})
     boolean setProfile(final String email,
                        final String tok,
