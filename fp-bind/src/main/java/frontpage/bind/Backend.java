2a0d66a44809396a3cd67cfbf6c0a71b5c771000
Will Stuckey <guyfleeman@gmail.com>
2016-11-12 20:20:34 -0500

    fp-bind now passing checkstyle

--- fp-bind/src/main/java/frontpage/bind/Backend.java
+++ fp-bind/src/main/java/frontpage/bind/Backend.java
@@ -7,14 +7,30 @@ import frontpage.bind.profile.ProfileManager;
 
 /**
  * @author willstuckey
- * <p></p>
+ * <p>Backend</p>
  */
 public interface Backend {
+    /**
+     * gets the user manager
+     * @return user manager
+     */
     UserManager getUserManager();
 
+    /**
+     * gets profile manager
+     * @return profile manager
+     */
     ProfileManager getProfileManager();
 
+    /**
+     * gets the source report manager
+     * @return source report manager
+     */
     SourceReportManager getSourceReportManager();
 
+    /**
+     * gets purity report manager
+     * @return purity report manager
+     */
     PurityReportManager getPurityReportManager();
 }
