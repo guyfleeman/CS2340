88fc1eb85f245583fd0f016a106a99692a9aaba2
Will Stuckey <guyfleeman@gmail.com>
2016-11-12 20:13:52 -0500

    Invalid Data Exception now passing checkstyle

--- fp-bind/src/main/java/frontpage/bind/errorhandling/InvalidDataException.java
+++ fp-bind/src/main/java/frontpage/bind/errorhandling/InvalidDataException.java
@@ -2,22 +2,38 @@ package frontpage.bind.errorhandling;
 
 /**
  * @author willstuckey
- * <p></p>
+ * <p>invalid Data Exception</p>
  */
 @SuppressWarnings({"WeakerAccess", "unused"})
 public class InvalidDataException extends BackendRequestException {
+    /**
+     * creates an invalid data exception
+     */
     public InvalidDataException() {
         super();
     }
 
+    /**
+     * creates an invalid data exception
+     * @param message message
+     */
     public InvalidDataException(final String message) {
         super(message);
     }
 
+    /**
+     * creates an invalid data exception
+     * @param t cause
+     */
     public InvalidDataException(final Throwable t) {
         super(t);
     }
 
+    /**
+     * creates an invalid data exception
+     * @param message message
+     * @param t cause
+     */
     public InvalidDataException(final String message, final Throwable t) {
         super(message, t);
     }
