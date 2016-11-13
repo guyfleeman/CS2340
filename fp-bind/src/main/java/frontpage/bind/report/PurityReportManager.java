6e6443a45e2943961ca4f30bb07d7f7aab9f0f3b
Will Stuckey <guyfleeman@gmail.com>
2016-11-12 20:02:09 -0500

    Purity Report Manager now passing checkstyle

--- fp-bind/src/main/java/frontpage/bind/report/PurityReportManager.java
+++ fp-bind/src/main/java/frontpage/bind/report/PurityReportManager.java
@@ -11,7 +11,10 @@ import java.util.Map;
 public interface PurityReportManager {
     /**
      * adds a blank purity report
+     * @param email email for auth
+     * @param tok token for auth
      * @return report UUID
+     * @throws BackendRequestException if things go wrong
      */
     String addPurityReport(final String email,
                            final String tok)
@@ -19,9 +22,12 @@ public interface PurityReportManager {
 
     /**
      * updates a purity report
+     * @param email email for auth
+     * @param tok token for auth
      * @param id UUID of source report
      * @param properties report properties to be written
      * @return success
+     * @throws BackendRequestException if things go wrong
      */
     @SuppressWarnings("UnusedReturnValue")
     boolean updatePurityReport(final String email,
@@ -34,27 +40,33 @@ public interface PurityReportManager {
      * gets purity report from UUID
      * @param id UUID of source report
      * @return report properties
+     * @throws BackendRequestException if things go wrong
      */
     Map<String, String> getPurityReport(final String id)
             throws BackendRequestException;
 
     /**
      * gets most recent purity reports
+     * @param num report history length
      * @return array containing one map describing each result
+     * @throws BackendRequestException if things go wrong
      */
     @SuppressWarnings({"SameParameterValue", "UnusedParameters"})
     Map<String, String>[] getPurityReports(final int num)
             throws BackendRequestException;
 
     /**
-     * gets purity reports based on a set of search constraints applied to properties
+     * gets purity reports based on a set of search constraints
+     * applied to properties
      * @param properties properties to search for
      * @param searchConstraints constraints for properties
      * @return array containing one map describing each result
+     * @throws BackendRequestException if things go wrong
      */
     @SuppressWarnings({"RedundantThrows", "UnusedParameters"})
-    Map<String, String>[] getPurityReports(final Map<String, String> properties,
-                                           final Map<String, String> searchConstraints)
+    Map<String, String>[] getPurityReports(
+            final Map<String, String> properties,
+            final Map<String, String> searchConstraints)
             throws BackendRequestException;
 
     /**
@@ -80,7 +92,9 @@ public interface PurityReportManager {
      * @param tok token for auth
      * @param id report id
      */
+    //CHECKSTYLE.OFF: MethodName
     void __deletePurityReport_fs_na(final String email,
                                     final String tok,
                                     final String id);
+    //CHECKSTYLE.ON: MethodName
 }
